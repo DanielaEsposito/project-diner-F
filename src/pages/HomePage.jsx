e49087ee7a19110a7ed2d61334a590b68d14533c
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-02-19 15:35:41 +0100

    add first home page structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,35 @@
 export default function HomePage() {
-  return <h1>homepage</h1>;
+  return (
+    <div className="wrapper">
+      <section id="hero-section">
+        <div className="container">
+          <div className="title-container">
+            <h1 className="top-bun">TOP BUN</h1>
+            <h3>EVEN THE BEST PILOTS NEED TO REFUEL</h3>
+          </div>
+        </div>
+      </section>
+      <section id="menu-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-9"></div>
+            <div className="col-4">
+              <h2 className="section-title">Menu</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+                unde laborum suscipit qui. Beatae maiores quo fugit quam dolor
+                veritatis rem expedita! Ipsa consequatur ipsam quo harum nisi,
+                quae placeat!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="events-section">
+        <div className="contaioner">
+          <div className="carosel-container"></div>
+        </div>
+      </section>
+    </div>
+  );
 }
