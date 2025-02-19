996d428e6f94b6d8f5e2a64c534c4e5db49b748e
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-02-19 17:53:10 +0100

    add.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -62,7 +62,45 @@ export default function HomePage() {
       </section>
       <section id="events-section">
         <div className="contaioner">
-          <div className="carosel-container"></div>
+          <div className="carousel-container">
+            <div id="carouselExample" className="carousel slide">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src="..." className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="..." className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="..." className="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
         </div>
       </section>
     </div>
