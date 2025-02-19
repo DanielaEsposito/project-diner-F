47d7db74581112ecbf7fb9f96cf8227f41261091
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-02-19 16:47:30 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,68 @@
 export default function Footer() {
-  return;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row ">
+          <div className="col-6 text-center">
+            <ul>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 text-center">
+            <ul>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="text-light">
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
